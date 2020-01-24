eeb833faa975d0790c35029d0eddf91d4885f6a7
uladzislau.cherak <uladzislau.cherak@itechart-group.com>
2020-01-24 15:45:25 +0300

    initial commit

---
+++ OAuth20/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OAuth20.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
